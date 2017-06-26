15d7135ac904be21de317782cf9b5c2aff9fc6f0
桐铭 梁 <497562602@163.com>
2017-06-26 17:25:38 +0800

    完善授权信息。

---
+++ src/YoYoCms.AbpProjectTemplate.WebApi/WebApi/Models/AuthenticateResultModel.cs
@@ -0,0 +1,23 @@
+﻿namespace YoYoCms.AbpProjectTemplate.WebApi.Models
+{
+
+    /// <summary>
+    /// 认证结果信息
+    /// </summary>
+    public class AuthenticateResultModel
+    {
+        /// <summary>
+        /// token值
+        /// </summary>
+        public string AccessToken { get; set; }
+         
+        /// <summary>
+        /// 剩余秒数
+        /// </summary>
+        public int ExpireInSeconds { get; set; }
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public long UserId { get; set; }
+    }
+}
