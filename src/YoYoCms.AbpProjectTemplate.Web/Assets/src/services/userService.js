9a14e77913ecbcff1c774310b8ca89566c34269f
hhx <249803023@qq.com>
2017-06-29 16:05:49 +0800

    修改登录逻辑

--- src/YoYoCms.AbpProjectTemplate.Web/Assets/src/services/userService.js
+++ src/YoYoCms.AbpProjectTemplate.Web/Assets/src/services/userService.js
@@ -3,10 +3,24 @@
  */
 //
 import fileService from './fileService'
-
+import apiHelper from './apiHelper'
 let userService = abp.services.app.user
 userService.exportExcel = async function () {
     let ret = await userService.getUsersToExcel()
     fileService.downloadTempFile(ret)
+}
+
+userService.login = async function (params) {
+    // {
+    //     url: `/Account/Login?returnUrl=/Application`,
+    //         methods: 'post',
+    //     data: this.fetchParam,
+    //     contentType: 'application/x-www-form-urlencoded'
+    // }
+    params.returnUrlHash = '#!/h'
+    let ret = await apiHelper.post('/Account/Login?returnUrl=none', params, {
+        contentType: 'application/x-www-form-urlencoded'
+    })
+    return ret
 }
 export default userService
