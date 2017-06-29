184f2861ca62484b62c464e425be7e692dbb5e04
桐铭 梁 <497562602@163.com>
2017-06-29 12:56:11 +0800

    添加一个APP，类库，用于动态生成的webapi项目。

---
+++ src/YoYoCms.AbpProjectTemplate.App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YoYoCms.AbpProjectTemplate.App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
