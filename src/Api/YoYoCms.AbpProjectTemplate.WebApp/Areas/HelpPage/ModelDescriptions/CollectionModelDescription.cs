1a4dad1093a719e872767faccaa7aacc0c917888
桐铭 梁 <497562602@163.com>
2017-06-29 16:07:04 +0800

    加入webapp

---
+++ src/Api/YoYoCms.AbpProjectTemplate.WebApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace YoYoCms.AbpProjectTemplate.WebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/Api/YoYoCms.AbpProjectTemplate.WebApp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace YoYoCms.AbpProjectTemplate.WebApp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
