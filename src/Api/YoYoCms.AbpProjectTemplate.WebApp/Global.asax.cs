40ed1acb3c18cf6cf0a92dba1da606fadfb9ee16
桐铭 梁 <497562602@163.com>
2017-06-29 16:28:00 +0800

    合并代码信息。

--- src/Api/YoYoCms.AbpProjectTemplate.WebApp/Global.asax.cs
+++ src/Api/YoYoCms.AbpProjectTemplate.WebApp/Global.asax.cs
@@ -6,18 +6,29 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Abp;
+using Abp.Castle.Logging.Log4Net;
+using Abp.Timing;
+using Abp.Web;
+using Castle.Facilities.Logging;
 
 namespace YoYoCms.AbpProjectTemplate.WebApp
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : AbpWebApplication<AbpProjectTemplateWebAppModule>
     {
-        protected void Application_Start()
+        protected override void Application_Start(object sender, EventArgs e)
         {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Use UTC clock. Remove this to use local time for your application.
+             
+            //Log4Net configuration
+            AbpBootstrapper.IocManager.IocContainer
+                .AddFacility<LoggingFacility>(f => f.UseAbpLog4Net()
+                    .WithConfig("log4net.config")
+                );
+
+            base.Application_Start(sender, e);
         }
+
+       
     }
 }
