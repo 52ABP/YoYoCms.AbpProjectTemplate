04dffc2c2403287d689efc5ccfae56518609947d
桐铭 梁 <497562602@163.com>
2017-06-30 12:08:19 +0800

    添加动态webapi，拦截和过滤功能。

---
+++ src/YoYoCms.AbpProjectTemplate.Application/AppExtensions/Attribute/WebAppApiAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace YoYoCms.AbpProjectTemplate.AppExtensions.Attribute
+{
+    /// <summary>
+    /// 使用该特性进行app过滤
+    ///  
+    /// </summary>
+    public class WebAppApiAttribute:System.Attribute
+    {
+        
+    }
+}
