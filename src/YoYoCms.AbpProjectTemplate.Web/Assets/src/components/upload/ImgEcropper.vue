bb97369b955e21e710bb0c5a9f0ad4852e453ddf
hhx <249803023@qq.com>
2017-06-30 10:43:54 +0800

    增加图片裁切组件

---
+++ src/YoYoCms.AbpProjectTemplate.Web/Assets/src/components/upload/ImgEcropper.vue
@@ -0,0 +1,21 @@
+<style rel="styleesheet" lang="scss">
+</style>
+
+<template>
+    <article>
+    </article>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+        },
+        activated() {
+        },
+        methods: {},
+        components: {}
+    }
+</script>
---
+++ src/YoYoCms.AbpProjectTemplate.Web/Assets/src/views/components/DialogEditMe.vue
@@ -0,0 +1,41 @@
+<!--首页修改用户信息的弹出框-->
+<style rel="styleesheet" lang="scss">
+</style>
+
+<template>
+    <el-dialog
+            title="我的信息"
+            :visible.sync="dialogVisible"
+            size="tiny">
+
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+          </span>
+    </el-dialog>
+</template>
+
+<script>
+    export default {
+        props: {
+            user: this.$store.state.auth.user
+        },
+        data() {
+            return {}
+        },
+        watch: {
+            'visible' (val) {
+                if (val != this.dialogVisible) this.dialogVisible = val
+            },
+            'dialogVisible' (val) {
+                this.$emit('update:visible', val)
+            }
+        },
+        created() {
+        },
+        activated() {
+        },
+        methods: {},
+        components: {}
+    }
+</script>
