511e7a658819ce22dc5faae69b0703df7f48d824
hhx <249803023@qq.com>
2017-07-12 09:26:49 +0800

    优化样式问题

--- src/YoYoCms.AbpProjectTemplate.Web/Assets/src/views/Test.vue
+++ src/YoYoCms.AbpProjectTemplate.Web/Assets/src/views/Test.vue
@@ -3,36 +3,27 @@
 
 <template>
     <article>
-        <Select2 :data="data" v-model="val"></Select2>
-        <el-button @click="changeData">改变值</el-button>
-
-        <el-button @click="changeSelected">改变选中</el-button>
+        <InputTest @keyup.enter.native="testfn" type="password"></InputTest>
     </article>
 </template>
 
 <script>
-    import Select2 from '../components/select/common/Select2.vue'
+    import InputTest from '../components/input/TestInput.vue'
     export default {
         data() {
-            return {
-                data: [{key: '1', value: 1}, {key: '2', value: 2}, {key: '222', value: 222}],
-                val: 2
-            }
+            return {}
         },
         created() {
         },
         activated() {
         },
         methods: {
-            changeData() {
-                this.data.push({key: '3', value: 3})
-            },
-            changeSelected () {
-                this.val = 1
+            testfn (val) {
+                console.log(val)
             }
         },
         components: {
-            Select2
+            InputTest
         }
     }
 </script>
