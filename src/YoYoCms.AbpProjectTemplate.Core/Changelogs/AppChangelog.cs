0621ce1dc8b687ab29f545676636553c5f4f32ae
桐铭 梁 <497562602@163.com>
2017-07-17 13:41:37 +0800

    添加系统更新日志功能。

---
+++ src/YoYoCms.AbpProjectTemplate.Core/Changelogs/AppChangelog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace YoYoCms.AbpProjectTemplate.Changelogs
+{
+    /// <summary>
+    /// 系统更新日志
+    /// </summary>
+    public class AppChangelog:CreationAuditedEntity<long>
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime ChangeTime { get; set; }
+
+        /// <summary>
+        /// 更新内容
+        /// </summary>
+        public string ChangeContent { get; set; }
+
+
+
+
+    }
+}
